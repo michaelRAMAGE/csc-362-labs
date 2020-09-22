7b86a3f8375aa03100b1b56e895d6feeba14f6c2
Michael L. Ramage <michael.ramage@centre.edu>
2020-09-22 16:02:02 +0000

    added comments

--- html/connect.php
+++ html/connect.php
@@ -1,15 +1,20 @@
-
-
 <?php
+
+    // error outputting
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
+    // credentials for db connection object
     $dbhost = 'localhost';
     $dbuser = 'ming';
     $dbpass = 'password'; 
 
+
+    // create db connection object 
     $conn = new mysqli($dbhost, $dbuser, $dbpass); 
+    
+    // error handling for db connection 
     if ($conn->connect_errno) {
         echo "Error: Failed to make a MySQL connection,
         here is why: ". "<br>";
@@ -21,28 +26,29 @@
         echo "YAY!" . "<br><br>";
     } 
 
-    if (isset($_POST['name'])) {
+    // check if post is non-null (set)
+    if (isset($_POST['name'])) {     // post has been set 
         echo $_POST['name']."'s tables are listed below<br>";
         $req_dbname = $_POST['name']; // get post val from _POST superglob assoc array
         $dblist = "SHOW TABLES FROM ".$req_dbname.";"; // construct query to be requested
-        if (!$conn->query($dblist)) { // error handling
+        if (!$conn->query($dblist)) { // query generated error, handle
             printf("Error message: %s\n", $conn->error);
         }
         else {
             $result = $conn->query($dblist); // request query 
             while ($table = $result->fetch_array()) { // fetch_array returns row array,                                           
-                echo $table[0] . "<br>";              // table[idx] is first element of row array
+                echo $table[0] . "<br>"; // table[idx] is first element of row array
             } 
         }
     } 
-    else {
+    else { // post has not yet been set
         echo "Type in a database to list its tables<br>";
     }
-
     $conn->close(); 
-
 ?> 
 
+<!-- FORM FOR SUBMITTING DATABASE NAME, WHERE TABLES 
+    WILL BE LISTED -->
 <html>
     <body>
         <form action="connect.php" method="post">
---
+++ html/details.php
@@ -0,0 +1,10 @@
+<!-- FORM FOR SUBMITTING DATABASE NAME, WHERE TABLES 
+    WILL BE LISTED -->
+<html>
+    <body>
+        <form action="connect.php" method="post">
+            Database Name: <input type="text" name="name"><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
